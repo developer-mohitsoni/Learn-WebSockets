0fc14c28b1a793c6ee689d0d958a38995581a8c7
Mohit Soni <developer_mohitsoni@outlook.com>
2025-05-05 18:22:37 +0530

    Refactor index.js to use ES module syntax for imports

--- by Piyush/Chat-App/index.js
+++ by Piyush/Chat-App/index.js
@@ -1,7 +1,7 @@
-const http = require("http");
-const express = require("express");
+import http from "node:http";
+import express from "express";
 
-const { Server } = require("socket.io");
+import { Server } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
@@ -9,24 +9,24 @@ const io = new Server(server);
 
 // Socket.io
 io.on("connection", (socket) => {
-  //   console.log("A new user has connected", socket.id);
+	//   console.log("A new user has connected", socket.id);
 
-  socket.on("myMessage", (message) => {
-    // console.log("A new user message", message);
+	socket.on("myMessage", (message) => {
+		// console.log("A new user message", message);
 
-    // Ab ye server iss "myMessage" mai jo bhi message aa raha hoga wo sabb users ko emit kar dega mtlb show karega.
+		// Ab ye server iss "myMessage" mai jo bhi message aa raha hoga wo sabb users ko emit kar dega mtlb show karega.
 
-    // Taaki jo bhi users iss message ko listen karna chayega wo easily iss message ko listen kar skta hai jaise:- sockets.on("message", (message)=>{}) on frontend side.
-    io.emit("message", message);
-  });
+		// Taaki jo bhi users iss message ko listen karna chayega wo easily iss message ko listen kar skta hai jaise:- sockets.on("message", (message)=>{}) on frontend side.
+		io.emit("message", message);
+	});
 });
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  return res.sendFile("index.html");
+	return res.sendFile("index.html");
 });
 
 server.listen(3000, () => {
-  console.log("Server is running on port 3000");
+	console.log("Server is running on port 3000");
 });
