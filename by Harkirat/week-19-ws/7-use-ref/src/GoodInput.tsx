c57330343463f9672fcceb0130377154699cece1
Mohit Soni <developer_mohitsoni@outlook.com>
2025-05-05 20:53:48 +0530

    Refactor GoodInput and main components for improved code clarity and structure

--- by Harkirat/week-19-ws/7-use-ref/src/GoodInput.tsx
+++ by Harkirat/week-19-ws/7-use-ref/src/GoodInput.tsx
@@ -1,17 +1,19 @@
-import React, {useRef} from "react";
+import { useRef } from "react";
 
 export const GoodInput = () => {
-    const inputRef = useRef(null);
+	const inputRef = useRef<HTMLInputElement>(null);
 
-    const handleButtonClick = () => {
-        inputRef.current.focus();
-        console.log(inputRef.current);
-    };
+	const handleButtonClick = () => {
+		inputRef.current?.focus();
+		console.log(inputRef.current);
+	};
 
-    return (
-        <div>
-            <input ref={inputRef} type="text" />
-            <button onClick={handleButtonClick}>Focus Input</button>
-        </div>
-    );
-}
+	return (
+		<div>
+			<input ref={inputRef} type="text" />
+			<button type="button" onClick={handleButtonClick}>
+				Focus Input
+			</button>
+		</div>
+	);
+};
