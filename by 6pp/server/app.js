b1c2f83524bcb63a061d252642f061c00f9fef3c
Mohit Soni <developer_mohitsoni@outlook.com>
2025-05-05 19:14:13 +0530

    Refactor App component and server setup for improved readability and consistency

--- by 6pp/server/app.js
+++ by 6pp/server/app.js
@@ -1,7 +1,7 @@
+import { createServer } from "node:http";
+import cors from "cors";
 import express from "express";
 import { Server } from "socket.io";
-import { createServer } from "http";
-import cors from "cors";
 import "dotenv/config";
 
 const app = express();
@@ -9,34 +9,34 @@ const app = express();
 const server = createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"],
-  },
+	cors: {
+		origin: "http://localhost:5173",
+		methods: ["GET", "POST"],
+		credentials: true,
+		allowedHeaders: ["Content-Type", "Authorization"],
+	},
 });
 
 app.use(express.json());
 app.use(
-  express.urlencoded({
-    extended: false,
-  })
+	express.urlencoded({
+		extended: false,
+	}),
 );
 
 const PORT = process.env.PORT || 3000;
 
 app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
+	cors({
+		origin: "http://localhost:5173",
+		methods: ["GET", "POST"],
+		credentials: true,
+		allowedHeaders: ["Content-Type", "Authorization"],
+	}),
 );
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+	res.send("Hello, World!");
 });
 
 const user = false;
@@ -52,36 +52,36 @@ io.use((socket, next) => {
 
 //! End if middleware
 io.on("connection", (socket) => {
-  console.log("User Connected", socket.id);
-  // socket.emit("welcome", `Welcome to the server`);
-  // socket.broadcast.emit("welcome", `${socket.id} joined the server`);
+	console.log("User Connected", socket.id);
+	// socket.emit("welcome", `Welcome to the server`);
+	// socket.broadcast.emit("welcome", `${socket.id} joined the server`);
 
-  socket.on("message", (data) => {
-    // console.log(data);
+	socket.on("message", (data) => {
+		// console.log(data);
 
-    // io.emit("receive-message", data)
-    // socket.broadcast.emit("receive-message", data)
+		// io.emit("receive-message", data)
+		// socket.broadcast.emit("receive-message", data)
 
-    console.log(data.room, data.message);
+		console.log(data.room, data.message);
 
-    io.to(data.room).emit("receive-message", data.message);
+		io.to(data.room).emit("receive-message", data.message);
 
-    //* These both doesn't care at this case
-    // socket.to(data.room).emit("receive-message", data.message);
+		//* These both doesn't care at this case
+		// socket.to(data.room).emit("receive-message", data.message);
 
-    socket.on("join-room", (room) => {
-      socket.join(room);
+		socket.on("join-room", (room) => {
+			socket.join(room);
 
-      console.log(`User joined room ${room}`);
-    });
-  });
+			console.log(`User joined room ${room}`);
+		});
+	});
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
-    // socket.broadcast.emit("goodbye", `${socket.id} left the server`);
-  });
+	socket.on("disconnect", () => {
+		console.log("User disconnected");
+		// socket.broadcast.emit("goodbye", `${socket.id} left the server`);
+	});
 });
 
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
 });
